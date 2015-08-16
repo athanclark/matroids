2c44bed06c37888670fdef51940f50dfb19f7dda
Athan Clark <athan.clark@gmail.com>
2015-08-16 13:21:22 -0600

    pivoting finished

--- src/Data/Matroid.hs
+++ src/Data/Matroid.hs
@@ -6,10 +6,15 @@
 
 module Data.Matroid where
 
-import Data.Set.Class
+import Data.Maybe (fromJust, isNothing)
+import Data.Set.Class as Sets
+import Data.Set.Ordered.Many.With
+import qualified Data.Map as Map
+
 import Data.Commutative
 import Data.Foldable as Fold
 import Control.Monad.Reader
+import Control.Monad.State
 
 
 data Matroid c a = Matroid
@@ -27,7 +32,7 @@ closure :: ( MonadReader (Matroid c a) m
            ) => c a -> m (c a)
 closure s = do
   d <- dependents <$> ask
-  return $ s `union` d
+  return $ s `Sets.union` d
 
 -- | Returns the size of the maximum basis that's a subset of the input.
 rank :: ( MonadReader (Matroid c a) m
@@ -38,8 +43,8 @@ rank :: ( MonadReader (Matroid c a) m
         , Fold.Foldable c
         ) => c a -> m Int
 rank s = do
-  b <- (foldr union empty . bases) <$> ask
-  return $ size $ s `intersection` b
+  b <- (Fold.foldr Sets.union Sets.empty . bases) <$> ask
+  return $ Sets.size $ s `Sets.intersection` b
 
 -- * Creation
 
@@ -48,8 +53,8 @@ mkMatroid :: ( Fold.Foldable c
              , HasDifference (c a)
              , HasUnion (c a)
              ) => c a -> c (c a) -> Matroid c a
-mkMatroid e bs = let b = unions bs in
-  Matroid bs $ e `difference` b
+mkMatroid e bs = let b = Sets.unions bs in
+  Matroid bs $ e `Sets.difference` b
 
 -- * SubMatroids and SuperMatroids
 
@@ -58,23 +63,66 @@ shrink :: ( HasIntersection (c a)
           ) => Matroid c a
             -> c a -- Smaller ground set
             -> Matroid c a
-shrink (Matroid bs ds) sub = Matroid ((`intersection` sub) <$> bs) $
-                                ds `intersection` sub
+shrink (Matroid bs ds) sub = Matroid ((`Sets.intersection` sub) <$> bs) $
+                                ds `Sets.intersection` sub
 
 grow :: ( HasUnion (c a)
         ) => Matroid c a
           -> c a -- New ground set
           -> c (c a) -- New bases
           -> Matroid c a
-grow (Matroid _ ds) sup bs' = Matroid bs' $ ds `union` sup
+grow (Matroid _ ds) sup bs' = Matroid bs' $ ds `Sets.union` sup
 
 -- * Greedy Algorithm
 
 -- | Dummy class to document that @<~>@ should /increase/ strictly positive.
 class Commutative c => CommutativePositive c where
 
-greedy :: ( MonadReader (Matroid c a) m
-          , CommutativePositive w
-          , Foldable c
-          , Ord a
-          ) => (a -> w) -> m
+takeMaximum :: ( Foldable c
+               , Eq (c a)
+               , Ord a
+               , Ord k
+               , Sets.HasEmpty (c a)
+               , Sets.HasDelete a (c a)
+               ) => SetsWith k c a -> (Maybe a, SetsWith k c a)
+takeMaximum xss@(SetsWith (f,xs)) = if Map.size xs > 0
+  then let (k, set) = Map.findMax xs
+           x = Fold.maximum set
+
+           xs' = Map.alter go k xs
+           go Nothing = Nothing
+           go (Just set') = if Sets.delete x set' == Sets.empty
+                            then Nothing
+                            else Just $ Sets.delete x set'
+       in (Just x, SetsWith (f,xs'))
+  else (Nothing, xss)
+
+pivot :: ( MonadState (SetsWith w c a) m
+         , MonadReader (Matroid c a) m
+         , CommutativePositive w
+         , Foldable c
+         , Eq (c a)
+         , Ord a
+         , Ord w
+         , Sets.HasEmpty (c a)
+         , Sets.HasDelete a (c a)
+         , Sets.HasInsert a (c a)
+         , Sets.CanBeSubset (c a)
+         ) => c a -> m (c a)
+pivot old = do
+  temp <- get
+  let (mx,temp') = takeMaximum temp
+  put temp'
+  bs <- bases <$> ask
+  if any (Sets.insert (fromJust mx) old `Sets.isSubsetOf`) bs
+  then return $ Sets.insert (fromJust mx) old
+  else return old
+
+--
+-- greedy :: ( MonadReader (Matroid c a) m
+--           , CommutativePositive w
+--           , Foldable c
+--           , Ord a
+--           , HasEmpty (c a)
+--           , HasDifference (c a)
+--           ) => (a -> w) -> m
