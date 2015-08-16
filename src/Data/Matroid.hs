a98b2052cc280a1d76455524a730ec3942069727
Athan Clark <athan.clark@gmail.com>
2015-08-16 16:08:55 -0600

    sucessful comiplation

--- src/Data/Matroid.hs
+++ src/Data/Matroid.hs
@@ -8,11 +8,13 @@ module Data.Matroid where
 
 import Data.Maybe (fromJust, isNothing)
 import Data.Set.Class as Sets
-import Data.Set.Ordered.Many.With
+import Data.Set.Ordered.Many.With (SetsWith (..))
+import qualified Data.Set.Ordered.Many.With as W
 import qualified Data.Map as Map
 
 import Data.Commutative
 import Data.Foldable as Fold
+import Data.Function.AlmostFix
 import Control.Monad.Reader
 import Control.Monad.State
 
@@ -26,13 +28,19 @@ deriving instance (Show (c a), Show (c (c a))) => Show (Matroid c a)
 deriving instance (Eq (c a), Eq (c (c a))) => Eq (Matroid c a)
 
 
+ground :: ( Foldable c
+          , HasEmpty (c a)
+          , HasUnion (c a)
+          ) => Matroid c a -> c a
+ground (Matroid bs ds) = unions bs `union` ds
+
 -- | Includes all spans (dependent elements) while keeping the same rank.
 closure :: ( MonadReader (Matroid c a) m
            , HasUnion (c a)
            ) => c a -> m (c a)
 closure s = do
   d <- dependents <$> ask
-  return $ s `Sets.union` d
+  return $ s `union` d
 
 -- | Returns the size of the maximum basis that's a subset of the input.
 rank :: ( MonadReader (Matroid c a) m
@@ -43,8 +51,8 @@ rank :: ( MonadReader (Matroid c a) m
         , Fold.Foldable c
         ) => c a -> m Int
 rank s = do
-  b <- (Fold.foldr Sets.union Sets.empty . bases) <$> ask
-  return $ Sets.size $ s `Sets.intersection` b
+  b <- (Fold.foldr union empty . bases) <$> ask
+  return $ size $ s `intersection` b
 
 -- * Creation
 
@@ -53,8 +61,8 @@ mkMatroid :: ( Fold.Foldable c
              , HasDifference (c a)
              , HasUnion (c a)
              ) => c a -> c (c a) -> Matroid c a
-mkMatroid e bs = let b = Sets.unions bs in
-  Matroid bs $ e `Sets.difference` b
+mkMatroid e bs = let b = unions bs in
+  Matroid bs $ e `difference` b
 
 -- * SubMatroids and SuperMatroids
 
@@ -63,15 +71,15 @@ shrink :: ( HasIntersection (c a)
           ) => Matroid c a
             -> c a -- Smaller ground set
             -> Matroid c a
-shrink (Matroid bs ds) sub = Matroid ((`Sets.intersection` sub) <$> bs) $
-                                ds `Sets.intersection` sub
+shrink (Matroid bs ds) sub = Matroid ((`intersection` sub) <$> bs) $
+                                ds `intersection` sub
 
 grow :: ( HasUnion (c a)
         ) => Matroid c a
           -> c a -- New ground set
           -> c (c a) -- New bases
           -> Matroid c a
-grow (Matroid _ ds) sup bs' = Matroid bs' $ ds `Sets.union` sup
+grow (Matroid _ ds) sup bs' = Matroid bs' $ ds `union` sup
 
 -- * Greedy Algorithm
 
@@ -91,9 +99,9 @@ takeMaximum xss@(SetsWith (f,xs)) = if Map.size xs > 0
 
            xs' = Map.alter go k xs
            go Nothing = Nothing
-           go (Just set') = if Sets.delete x set' == Sets.empty
+           go (Just set') = if delete x set' == empty
                             then Nothing
-                            else Just $ Sets.delete x set'
+                            else Just $ delete x set'
        in (Just x, SetsWith (f,xs'))
   else (Nothing, xss)
 
@@ -111,18 +119,29 @@ pivot :: ( MonadState (SetsWith w c a) m
          ) => c a -> m (c a)
 pivot old = do
   temp <- get
-  let (mx,temp') = takeMaximum temp
+  let (mx,temp') = takeMaximum temp -- deletes
   put temp'
   bs <- bases <$> ask
-  if any (Sets.insert (fromJust mx) old `Sets.isSubsetOf`) bs
-  then return $ Sets.insert (fromJust mx) old
+  if any (insert (fromJust mx) old `isSubsetOf`) bs
+  then return $ insert (fromJust mx) old
   else return old
 
---
--- greedy :: ( MonadReader (Matroid c a) m
---           , CommutativePositive w
---           , Foldable c
---           , Ord a
---           , HasEmpty (c a)
---           , HasDifference (c a)
---           ) => (a -> w) -> m
+greedy :: ( MonadFix m
+          , MonadReader (Matroid c a) m
+          , CommutativePositive w
+          , Foldable c
+          , Eq (c a)
+          , Ord a
+          , Ord w
+          , Sets.HasEmpty (c a)
+          , Sets.HasDelete a (c a)
+          , Sets.HasInsert a (c a)
+          , Sets.CanBeSubset (c a)
+          , Sets.HasUnion (c a)
+          , Sets.HasSingleton a (c a)
+          ) => (a -> w) -> m (c a)
+greedy f = do
+  g <- ground <$> ask
+  let r = almostFixM (liftM (/= W.empty f) get) pivot
+      w = W.fromList f g
+  evalStateT (r Sets.empty) w
